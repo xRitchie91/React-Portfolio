60257e9c397c4cc0517755f56dfc67bfc38b8434
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-29 22:51:40 -0500

    Update footer.js
    updated links in footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,12 +6,14 @@ function Footer() {
         <footer>
             <h3 className="footer">
                 <center>
+                    <h2>Links</h2>
                 <ul>
                 | <a href="https://github.com/xRitchie91">GitHub</a> | 
                 | <a href="https://www.linkedin.com/in/richard-ortiz-6a8a82bb/">Linkedin</a> |
                 | <a href="mailto:r.ortiz54@yahoo.com">Email</a> | 
                 | <a href="tel:5126960674">Telephone</a> |
                 </ul>
+                <p>🤙🏽 Made by Ritchie</p>
                 </center>
 
             </h3>
