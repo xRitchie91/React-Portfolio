4386c3ad3477615f2957e83dac7e5ab2c77469f3
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-29 22:58:47 -0500

    removed non-working link/list

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -27,27 +27,7 @@ function Nav(props) {
                     <center>React Portfolio</center>
                 </a>
             </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                    </li>
-                    <li className={"mx-2"}>
-                        <span onClick={() => handleClick('Contact')}>Contact</span>
-                    </li>
-                    {categories.map((category) => (
-                        <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'
-                            }`} key={category.name}>
-                            <span
-                                onClick={() => {
-                                    setCurrentCategory(category)
-                                }}
-                            >
-                                {capitalizeFirstLetter(category.name)}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
+            
         </header >
     );
 }
