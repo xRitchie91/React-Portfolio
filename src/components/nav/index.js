8a75a832e4ec7638c05226b97c168f2e555ade95
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-12-15 00:13:41 -0600

    Updated nav links
    cleaned up nav links and fixed location. Also added new link for resume.

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -11,16 +11,19 @@ function Nav(props) {
             </h2>
             <h4>
                 <a data-testid="link" href="/about" className="about">
-                  <span role="img" aria-label="camera"></span>About</a>
+                  <span role="img" aria-label="camera"></span> About</a> • 
 
                   <a data-testid="link" href="/projects" className="projects">
-                  <span role="img" aria-label="camera"></span>Projects</a>
+                  <span role="img" aria-label="camera"></span> Projects</a> • 
 
                   <a data-testid="link" href="https://www.github.com/xRitchie91" className="github">
-                  <span role="img" aria-label="camera"></span>GitHub</a>
+                  <span role="img" aria-label="camera"></span> GitHub</a> • 
 
                   <a data-testid="link" href="https://www.linkedin.com/rportiz" className="linkedin">
-                  <span role="img" aria-label="camera"></span>Linkedin</a>
+                  <span role="img" aria-label="camera"></span> Linkedin</a> •
+
+                  <a data-testid="link" href="https://bit.ly/3m8hpaO" className="Resume">
+                  <span role="img" aria-label='camera'></span> Resume</a>
 
             </h4>
             </center>
